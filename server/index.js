be662c7194cc49fbc86b5324125b4eb15026266b
fakhiraadz <fakhiraadzlin@gmail.com>
2021-06-19 16:18:58 +0800

    change port

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const userRoute = require('./routes/Otp');
 app.use('/otp', userRoute);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
