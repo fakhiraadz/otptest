1e5c14e6e6e98734ddbcb7bd187a04dcdbbbfcaf
fakhiraadz <fakhiraadzlin@gmail.com>
2021-06-19 15:41:48 +0800

    change db connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'otpdb'
+  host     : 'eu-cdbr-west-01.cleardb.com',
+  user     : 'b66931577db9f1',
+  password : 'a1c4271b',
+  database : 'heroku_387537abc9826b2'
 });
 
 module.exports = db;
