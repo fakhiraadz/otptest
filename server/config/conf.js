533339521a6fcd983dbfc320bbdfd96f6197fcd3
fakhiraadz <fakhiraadzlin@gmail.com>
2021-06-20 01:05:45 +0800

    Update conf.js

--- server/config/conf.js
+++ server/config/conf.js
@@ -1,5 +1,5 @@
 module.exports = {
     serviceID: "VA72ca55768bc4b382ba3f23d19a7fd624",
     accountSID: "AC3736afb8352cb5665e8b527ecc22ec33",
-    authToken: "1a33c4a053d8ce43e14b2ef913975141"
+    authToken: "1a33c4a053d8ce43e14b2ef913975141",
 }
