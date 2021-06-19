144e0b36dbdf12a68e81da4c18a233f8377c75c2
fakhiraadz <fakhiraadzlin@gmail.com>
2021-06-19 18:23:40 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import Otpverify from './Otpverify';
 import VerifyPage from './VerifyPage';
 import Home from './Home'
 import Register from './Register';
+import Viewuser from './Viewuser';
 
 function App() {
 
 
   return (
     <>
-    
-    <Router>
-      <Route path="/" exact render={() => <VerifyPage />} />
-      <Route path="/otp" exact render={() => <Otpverify />} />
-      <Route path="/register" exact render={() => <Register />} />
-      <Route path="/home" exact render={() => <Home />} />
-    </Router>
+
+      <Router>
+        <Route path="/" exact render={() => <VerifyPage />} />
+        <Route path="/otp" exact render={() => <Otpverify />} />
+        <Route path="/register" exact render={() => <Register />} />
+        <Route path="/home" exact render={() => <Home />} />
+        <Route path="/viewuser" exact render={() => <Viewuser />} />
+      </Router>
     </>
   );
 }
